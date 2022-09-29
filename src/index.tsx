777991637edd9574874144dfe3312537702f10b5
Jose Albizures <albizures3601@gmail.com>
2022-09-29 16:42:09 -0600

    chore: export utils

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
 export { defaulStore, defaulStore as store } from './store';
 export * from './types';
 export { createItem, useSetItem, useItemValue, useItem } from './item';
+export * from './utils';
